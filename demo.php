f77ee9103c42020850dcd761fabee7ddbdc2bb13
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-07 19:06:29 +0100

    write

--- demo.php
+++ demo.php
@@ -1,1 +1,13 @@
 <?php
+
+$handle = fopen('objednavky.txt', 'r');
+if ($handle) {
+    while (($line = fgets($handle, 4096)) !== false) {
+        $exploded = explode(';', $line);
+
+        foreach ($exploded as $item) {
+            echo "$item | ";
+        }
+    }
+    echo fgets($handle, 4032);
+}
