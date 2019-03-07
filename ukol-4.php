3940fda323fd00a625523d3bef19d09757701749
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-07 20:11:31 +0100

    file

--- ukol-4.php
+++ ukol-4.php
@@ -1,5 +1,13 @@
 <?php
 function maximum($a, $b) {
-    echo max($a, $b);
+    if (is_numeric($a) and is_numeric($b)) {
+        echo max($a, $b);
+}   else {
+        echo "Neobsahuje cislo";
+    }
+
 }
-maximum();
+
+maximum(3,6);
+    maximum(305, 421);
+    maximum('x', 55);
