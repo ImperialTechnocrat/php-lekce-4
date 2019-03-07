188f5c920fcff39968c31842b863d601236d9de6
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-07 20:44:04 +0100

    file

--- ukoly-5.php
+++ ukoly-5.php
@@ -1,13 +1,3 @@
-/**
-
-1. Vytvořte soubor `ukol-``5``.php`
-2. Vytvořte funkci `minimum`, která spočítá minimum z 2 čísel zadaných jako argumenty
-3. Funkce vrátí výsledek (nejmenší číslo) pomocí příkazu `return`
-4. Pokud první nebo druhý parametr není číselný, funkce vrátí hodnotu `false`
-5. Funkci zavolejte s argumenty `56` a `12.3`, a výsledek vypište pomocí `echo`
-6. Ošetřete návratovou hodnotu `false` - pokud ji funkce vrátí, vypište chybu
-7. Změňte parametry na `56` a `'xy'`
-*/
 <?php
 function retrieve($a, $b) {
     if (is_numberic($a) and is_numeric($b)) {
@@ -18,3 +8,4 @@ function retrieve($a, $b) {
 
 }
 
+retrieve(5,4);
