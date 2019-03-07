211be7b16c4cedaf8e2811b2176dba3321be8e7c
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-07 18:44:00 +0100

    write

--- ukol-1.php
+++ ukol-1.php
@@ -1,6 +1,10 @@
 <?php
 
 echo 'Historie přístupů:<br>';
-$handle = fopen("pristupy.txt",  "w");
-$date = fileatime( $handle );
-echo $date;
+$handle = fopen("pristupy.txt",  "a");
+if ($handle === false) {
+    echo "File cannot be opened";
+}   else {
+    fwrite($handle, date('Y-m-d H:i:s') . "<br>\n");
+}
+echo fread($handle, 1000);
