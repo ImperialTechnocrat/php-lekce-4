32abdb5900a7cdc0055a5b248a80b8f1e5ca2fa2
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-11 23:06:17 +0100

    file

--- vlozit.php
+++ vlozit.php
@@ -1,9 +1,11 @@
 <?php
-$handle = fopen('prispevky.txt,', 'a') or die("Unable to open file");
-var_dump($_POST);
 $semicolonSeparation = implode(";", $_POST);
-echo $semicolonSeparation;
-fwrite($handle, $semicolonSeparation);
-echo "Uspesne ulozeno";
+$handle = fopen("prispevky.txt",  "a");
+if ($handle === false) {
+    echo "File cannot be opened";
+}   else {
+    fwrite($handle, $semicolonSeparation . "<br>\n");
+    echo "Uspesne ulozeno";
+}
 echo '<br><a href="navstevni-kniha.php">Navstevni kniha</a>';
 fclose($handle);
