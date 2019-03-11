a04406f7b09ccda802340c5c5c291ef3fd900ec4
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-11 22:21:13 +0100

    file

--- navstevni-kniha.php
+++ navstevni-kniha.php
@@ -17,7 +17,6 @@
     <form action="vlozit.php" method = "post">
     Jméno: <input type="text"name="firstName"><br>
     Heslo: <input type="password"name="secret"><br>
-    <input type="checkbox"name="optIn"> Souhlasím<br>
     <input type="submit"><br>
         <textarea name="message" rows="10" cols="30">The cat was playing in the garden.</textarea>
         <br>
--- vlozit.php
+++ vlozit.php
@@ -1,6 +1,3 @@
 <?php
 $handle = fopen('prispevky.txt,', 'a') or die("Unable to open file");
 var_dump($_POST);
-$entry = explode(';', $_POST);
-echo $entry;
-fwrite($handle, $_POST);
