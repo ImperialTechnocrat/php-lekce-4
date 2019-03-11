a63c490f4e7e5640e7a3ac699a91a4b583e19f1d
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-11 22:37:13 +0100

    file

--- vlozit.php
+++ vlozit.php
@@ -4,3 +4,5 @@ var_dump($_POST);
 $semicolonSeparation = implode(";", $_POST);
 echo $semicolonSeparation;
 fwrite($handle, $semicolonSeparation);
+echo "Uspesne ulozeno";
+echo '<br><a href="navstevni-kniha.php">Navstevni kniha</a>';
