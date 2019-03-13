5ee6eab078ee6f6c81addd91ad4c8b63be6c9a95
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-13 23:46:38 +0100

    file

--- vlozit.php
+++ vlozit.php
@@ -1,5 +1,4 @@
 <?php
-echo 'Historie přístupů:<br>';
 $inputtedText = implode(": ", $_POST);
 $handle = fopen("prispevky.txt",  "a");
 if ($handle === false) {
@@ -11,7 +10,7 @@ $handle = fopen("prispevky.txt",  "r");
 if ($handle === false) {
     echo "File cannot be opened";
 }   else {
-    echo fread($handle, 1000);
+    echo "Vzkaz ulozen";
 }
 echo '<br><a href="navstevni-kniha.php">Navstevni kniha</a>';
 fclose($handle);
